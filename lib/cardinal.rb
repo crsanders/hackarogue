e00328394ccb3f2c00089190663dc97acd0dfe37
Christian Sanders <csanders@camber.com>
2014-12-30 14:16:37 -0600

    Added Character Creation

--- lib/cardinal.rb
+++ lib/cardinal.rb
@@ -1,14 +1,32 @@
 # Cardinal handles anything and everything system side
 
 require 'json'
+require 'io/console'
 require_relative 'classlist'
 require_relative 'player'
 require_relative 'menu'
 
-class Cardinal
-  attr_accessor :player
+module Cardinal
 
-  def initialize
+  def clear
+    system("reset")
+  end
+
+  # Reads keypresses from the user including 2 and 3 escape character sequences.
+  def read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e"
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+    ensure
+      STDIN.echo = true
+      STDIN.cooked!
+
+    return input
   end
 
   # save_data should be JSON. pclass is an object from classlist
@@ -21,7 +39,7 @@ class Cardinal
       end
       @player = Player.new(save_data)
     else
-      @player = Player.new(pclass, name)
+      @player = Player.new(nil, pclass, name)
     end
   end
 end
--- lib/menu.rb
+++ lib/menu.rb
@@ -1,15 +1,76 @@
 # Contains all of the menus needed by the game
 
-class MainMenu
-  def initialize
+module Cardinal
+  class MainMenu
+    def initialize
+    end
+
+    def display_opening
+      clear
+      puts "Welcome to Hackarogue!\n\n\n"
+      print "Press any button to continue"
+      read_char
+    end
+
+    def display_options
+      options = %w(1 2 3 4)
+      clear
+      puts "1. Play\n2. Options\n3. Help\n4. Exit"
+      c = read_char until options.include? c
+      case c
+      when "1"
+        StartGameMenu.new
+      when "2"
+        puts "Options"
+      when "3"
+        puts "Help"
+      when "4"
+        puts "Exiting"
+      end
+    end
   end
 
-  def clear
-    system("reset")
+  class StartGameMenu
+    def initialize
+      clear
+      print "Do you have save data (y/n)? "
+      c = read_char until c == 'y' or c == 'n'
+      if c == 'y'
+        puts "To be completed"
+      else
+        ClassMenu.new
+      end
+    end
   end
 
-  def display_opening
-    clear
-    puts "Welcome to <title>"
+  class ClassMenu
+    def initialize
+      select_class
+      enter_name
+      create_player(nil, @class, @name)
+      puts @player.inspect
+    end
+
+    def select_class
+      clear
+      options = %w(1 2 3)
+      puts "Select your class\n"
+      puts "1. Squire\n2. Mage\n3. Archer"
+      c = read_char until options.include? c
+      case c
+      when "1"
+        @class = Squire.new
+      when "2"
+        @class = Mage.new
+      when "3"
+        @class = Archer.new
+      end
+    end
+
+    def enter_name
+      clear
+      print "Enter your name: "
+      @name = gets.chomp
+    end
   end
 end
